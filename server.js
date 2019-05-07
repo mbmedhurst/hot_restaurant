391f933f6b7f05c7318a669bb933383bf4ca6ec6
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-05-06 21:04:46 -0700

    files created

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const { join } = require('path')
+const app = express()
+
+app.use(express.static(join(__dirname, '/public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+const routes = require('./routes')
+
+routes(app)
+
+app.listen(process.env.PORT || 3000)
